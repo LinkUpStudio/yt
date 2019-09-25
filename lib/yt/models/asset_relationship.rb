b1b05aa0c23f38bee787e7b08bfb45f5c2e180f2
Pavel Bonda <bondek777@gmail.com>
2019-09-25 15:17:03 +0300

    Asset relationship. Add delete method

--- lib/yt/models/asset_relationship.rb
+++ lib/yt/models/asset_relationship.rb
@@ -1,12 +1,26 @@
 module Yt
   module Models
     class AssetRelationship < Base
-      attr_reader :auth
+      attr_reader :auth, :id
 
       def initialize(options = {})
         @data = options.fetch(:data, {})
-        @id = options[:id]
+        @id = @data['id']
         @auth = options[:auth]
+      end
+
+      def delete
+        do_delete
+        self
+      end
+
+      private
+
+      def modify_params
+        super.tap do |params|
+          params[:path] = "/youtube/partner/v1/assetRelationships/#{id}"
+          params[:params] = { on_behalf_of_content_owner: auth.owner_name }
+        end
       end
     end
   end
